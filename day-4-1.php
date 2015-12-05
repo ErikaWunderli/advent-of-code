6e14157286e60e4064d25aedbfe3911beaf9050a
Ed Hicks <ed@blucube.net>
2015-12-05 17:50:03 +0000

    Day 4

---
+++ day-4-1.php
@@ -0,0 +1,45 @@
+<?php
+	
+/*
+	
+--- Day 4: The Ideal Stocking Stuffer ---
+
+Santa needs help mining some AdventCoins (very similar to bitcoins) to use as gifts for all the economically forward-thinking little girls and boys.
+
+To do this, he needs to find MD5 hashes which, in hexadecimal, start with at least five zeroes. The input to the MD5 hash is some secret key (your puzzle input, given below) followed by a number in decimal. To mine AdventCoins, you must find Santa the lowest positive number (no leading zeroes: 1, 2, 3, ...) that produces such a hash.
+
+For example:
+
+If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
+If your secret key is pqrstuv, the lowest number it combines with to make an MD5 hash starting with five zeroes is 1048970; that is, the MD5 hash of pqrstuv1048970 looks like 000006136ef....
+
+*/
+
+
+// load key into string
+$key = file_get_contents('input/day-4.txt');
+
+// counter
+$n = 1;
+
+// loop until answer found
+$answer = false;
+while($answer == false)
+{
+	
+	// create hash
+	$hash = md5($key.$n);
+	
+	// check for match
+	if(substr($hash, 0, 5) === '00000') $answer = $n;
+	
+	// increment counter
+	$n++;
+	
+}
+
+// output answer
+echo 'Answer: '.$answer;
+
+
+?>
---
+++ day-4-2.php
@@ -0,0 +1,38 @@
+<?php
+	
+/*
+	
+--- Part Two ---
+
+Now find one that starts with six zeroes.
+
+*/
+
+
+// load key into string
+$key = file_get_contents('input/day-4.txt');
+
+// counter
+$n = 1;
+
+// loop until answer found
+$answer = false;
+while($answer == false)
+{
+	
+	// create hash
+	$hash = md5($key.$n);
+	
+	// check for match
+	if(substr($hash, 0, 6) === '000000') $answer = $n;
+	
+	// increment counter
+	$n++;
+	
+}
+
+// output answer
+echo 'Answer: '.$answer;
+
+
+?>
